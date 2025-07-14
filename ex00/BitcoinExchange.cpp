2822c471c15149eb4c4d61875940ef981aba14ca
17931837B <tomoya.bb1014@gmail.com>
2025-07-14 17:24:28 +0900

    bit fire!

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,1 +1,30 @@
-int is_digit
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+
+}
+
+BitcoinExchange::BitcoinExchange(const char *path)
+{
+	setData(path);
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy)
+{
+	*this = copy;
+}
+
+BitcoinExchange& BitcoinExchange::operator= (const BitcoinExchange &src)
+{
+	if (this == &src)
+		return (*this);
+	_bitInf = src._bitInf;
+	return (*this);
+}
+
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,12 +6,15 @@
 class BitcoinExchange
 {
 	private:
-		std::map<std::string, float> exchangeRates;
+		std::map<std::string, float> _bitInf;
 		BitcoinExchange();
 	public:
+		BitcoinExchange(const char *path);
 		~BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange &copy);
 		BitcoinExchange& operator = (const BitcoinExchange &src);
+		void	setData(const char *path);
+
 };
 
 #endif
